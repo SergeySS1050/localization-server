8392f92774f30beaa06754ccdb58d21a7b21546c
sakethpathike <sakethpathike@gmail.com>
2025-04-21 02:09:56 +0530

    feat: add `/keys` to retrieve the app's latest version's localization keys with default values.

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,6 +29,8 @@ dependencies {
     implementation("io.ktor:ktor-server-core-jvm")
     implementation("io.ktor:ktor-server-netty-jvm")
     implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("io.ktor:ktor-client-core:$kotlin_version")
+    implementation("io.ktor:ktor-client-cio-jvm:2.3.12")
     testImplementation("io.ktor:ktor-server-test-host-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
     implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:2.3.12")
--- src/main/kotlin/saketh/linkora/localization/routing/Routing.kt
+++ src/main/kotlin/saketh/linkora/localization/routing/Routing.kt
@@ -1,5 +1,9 @@
 package saketh.linkora.localization.routing
 
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
@@ -9,6 +13,7 @@ import saketh.linkora.localization.domain.Language
 import saketh.linkora.localization.domain.repository.LocalizationRepo
 
 fun Application.configureRouting(localizationRepo: LocalizationRepo) {
+    val json = Json { isLenient = true }
     routing {
         get("/") {
             call.respondRedirect("https://github.com/sakethpathike/Linkora")
@@ -21,7 +26,26 @@ fun Application.configureRouting(localizationRepo: LocalizationRepo) {
                 )
             )
         }
-
+        get("/keys") {
+            HttpClient(CIO).use { httpClient ->
+                httpClient.get("https://raw.githubusercontent.com/LinkoraApp/Linkora/master/composeApp/src/commonMain/kotlin/com/sakethh/linkora/common/Localization.kt")
+                    .bodyAsText().substringAfter("/***** THE SACRED SCRAPING RITUAL BEGINS *****/").substringBefore(
+                        "/*****  SCRAPING RITUAL COMPLETE  *****/"
+                    ).trimIndent().split("),").map {
+                        it.trim().run {
+                            if (this[this.lastIndex] == ')') this.substring(0, this.lastIndex) else this
+                        }
+                    }.associate {
+                        it.substringBefore("(") to json.decodeFromString<String>(
+                            it.substringAfter("(").substringAfter("defaultValue").substringAfter("=").trim()
+                                .replace("\\'", "'")
+                        ).replace("\${LinkoraPlaceHolder.First.value}", "{#LINKORA_PLACE_HOLDER_1#}")
+                            .replace("\${LinkoraPlaceHolder.Second.value}", "{#LINKORA_PLACE_HOLDER_2#}")
+                    }.let {
+                        call.respond(it)
+                    }
+            }
+        }
         Language.entries.forEach { language ->
             get(language.availableLanguageDTO.languageCode) {
                 call.respondText(localizationRepo.getTranslationsFor(language))
