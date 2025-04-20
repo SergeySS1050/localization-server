33640f3893c11e3ac9e4bba07383612afe588306
sakethpathike <sakethpathike@gmail.com>
2025-04-21 02:26:03 +0530

    feat: add ability to retrieve latest translations from the master branch of this repo

--- src/main/kotlin/saketh/linkora/localization/data/repository/LocalizationRepoImpl.kt
+++ src/main/kotlin/saketh/linkora/localization/data/repository/LocalizationRepoImpl.kt
@@ -1,11 +1,17 @@
 package saketh.linkora.localization.data.repository
 
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
 import kotlinx.serialization.json.Json
 import saketh.linkora.localization.domain.Language
 import saketh.linkora.localization.domain.model.info.LocalizedInfoDTO
 import saketh.linkora.localization.domain.repository.LocalizationRepo
 
 class LocalizationRepoImpl : LocalizationRepo {
+    val json = Json { isLenient = true }
+
     private fun retrieveRawFileString(languageCode: String): String {
         val file = this::class.java.getResourceAsStream("/raw/$languageCode.json")
         return file.use { it?.bufferedReader()?.readText().toString() }
@@ -28,4 +34,35 @@ class LocalizationRepoImpl : LocalizationRepo {
         )
     }
 
+    override suspend fun getLatestKeysWithDefaultValues(): Map<String, String> {
+        return HttpClient(CIO).use { httpClient ->
+            httpClient.get("https://raw.githubusercontent.com/LinkoraApp/Linkora/master/composeApp/src/commonMain/kotlin/com/sakethh/linkora/common/Localization.kt")
+                .bodyAsText().substringAfter("/***** THE SACRED SCRAPING RITUAL BEGINS *****/").substringBefore(
+                    "/*****  SCRAPING RITUAL COMPLETE  *****/"
+                ).trimIndent().split("),").map {
+                    it.trim().run {
+                        if (this[this.lastIndex] == ')') this.substring(0, this.lastIndex) else this
+                    }
+                }.associate {
+                    it.substringBefore("(") to json.decodeFromString<String>(
+                        it.substringAfter("(").substringAfter("defaultValue").substringAfter("=").trim()
+                            .replace("\\'", "'")
+                    ).replace("\${LinkoraPlaceHolder.First.value}", "{#LINKORA_PLACE_HOLDER_1#}")
+                        .replace("\${LinkoraPlaceHolder.Second.value}", "{#LINKORA_PLACE_HOLDER_2#}")
+                }
+        }
+    }
+
+    override suspend fun getLatestKeyValuePairsForALanguage(languageCode: String): Result<Map<String, String>> {
+        return HttpClient(CIO).use { httpClient ->
+            try {
+                httpClient.get("https://raw.githubusercontent.com/LinkoraApp/localization-server/master/src/main/resources/raw/$languageCode.json")
+                    .bodyAsText().run {
+                        Result.success(Json.decodeFromString(this))
+                    }
+            } catch (e: Exception) {
+                Result.failure(e)
+            }
+        }
+    }
 }
--- src/main/kotlin/saketh/linkora/localization/domain/repository/LocalizationRepo.kt
+++ src/main/kotlin/saketh/linkora/localization/domain/repository/LocalizationRepo.kt
@@ -6,4 +6,7 @@ import saketh.linkora.localization.domain.model.info.LocalizedInfoDTO
 interface LocalizationRepo {
     fun getTranslationsFor(language: Language): String
     fun getLocalizedInfo(): LocalizedInfoDTO
+
+    suspend fun getLatestKeysWithDefaultValues(): Map<String, String>
+    suspend fun getLatestKeyValuePairsForALanguage(languageCode: String): Result<Map<String, String>>
 }
