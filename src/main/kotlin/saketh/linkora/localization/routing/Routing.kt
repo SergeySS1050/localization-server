f921baa0cb2fe0323d3a9eb3423ea6e226826ae6
sakethpathike <sakethpathike@gmail.com>
2025-05-03 14:02:14 +0530

    fix(translation-editor): add viewport meta tag for proper scaling

--- src/main/kotlin/saketh/linkora/localization/routing/Routing.kt
+++ src/main/kotlin/saketh/linkora/localization/routing/Routing.kt
@@ -45,6 +45,15 @@ fun Application.configureRouting(localizationRepo: LocalizationRepo) {
             }.onSuccess {
                 call.respondText(contentType = ContentType.Text.Html, text = createHTML().html {
                     Surface(
+                        onTheHeadElement = {
+                            unsafe {
+                                raw(
+                                    """
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                """.trimIndent()
+                                )
+                            }
+                        },
                         fonts = listOf("https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap"),
                         style = {
                             unsafe {
