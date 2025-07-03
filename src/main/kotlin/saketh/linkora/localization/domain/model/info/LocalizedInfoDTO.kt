546ff8baec389c437b56f8295b1fac04c609dc63
sakethpathike <sakethpathike@gmail.com>
2025-07-03 21:33:25 +0530

    fix: use `totalStrings` as the serial name for `totalDefaultValues`

--- src/main/kotlin/saketh/linkora/localization/domain/model/info/LocalizedInfoDTO.kt
+++ src/main/kotlin/saketh/linkora/localization/domain/model/info/LocalizedInfoDTO.kt
@@ -1,10 +1,12 @@
 package saketh.linkora.localization.domain.model.info
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class LocalizedInfoDTO(
     val availableLanguages: List<AvailableLanguageDTO>,
+    @SerialName("totalStrings")
     val totalDefaultValues: Int,
     val lastUpdatedOn: String,
     val totalAvailableLanguages: Int = availableLanguages.size
